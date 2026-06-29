31173df2327414a0ad9b59d45770a9cc2d6ed887
StaticOS <yusuf1905emre1905@gmail.com>
2026-06-29 15:35:04 +0300

    Add AndroidProducts.mk for alioth device configuration

---
+++ AndroidProducts.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2026 The StaticOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from sm8250-common
+include device/xiaomi/sm8250-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/xiaomi/alioth
+
+# Board
+TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
+
+# Display
+TARGET_SCREEN_DENSITY := 440
+
+# Kernel
+TARGET_KERNEL_CONFIG += vendor/xiaomi/alioth.config
+
+# OTA assert
+TARGET_OTA_ASSERT_DEVICE := alioth,aliothin
+
+# Properties
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+
+# Inherit from the proprietary version
+include vendor/xiaomi/alioth/BoardConfigVendor.mk
